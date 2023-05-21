ed91eeba8f938a861bfcada91f6fec14d577e7f3
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-05-21 22:45:05 +0600

    added

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aj4jua7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  const dbConnect = async () => {
+    try {
+        await client.connect();
+        console.log("Database Connected successfully ✅");
+    } catch (error) {
+        console.log(error.name, error.message);
+    }
+}
+dbConnect();
+const toyCollection = client.db('toysDB').collection('toys');
+
+
+app.post('/addtoy', async (req,res) =>{
+    const toy = req.body;
+    console.log(toy)
+    const result = await toyCollection.insertOne(toy)
+    res.send(result)
+})
+
+
+app.get('/',(req,res) => {
+    res.send('Baby Mart is Running')
+})
+
+app.listen(port, () =>{
+    console.log(`Baby Mart Is Running in Port ${port}`)
+})
