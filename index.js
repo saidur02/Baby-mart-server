4ce43b49ac154bc3b8c00e841c5b869232ebf207
Md Saidur Rahman <saidurrahmanbanguri1997@gmail.com.com>
2023-06-01 02:14:20 +0600

    problem solve

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-const corsConfig = {
-    origin: "",
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-    }
-    app.use(cors(corsConfig))
-    app.options("", cors(corsConfig))
+
+app.use(cors())
 app.use(express.json());
 
 
@@ -20,99 +15,78 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aj4jua7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
-  });
-  const dbConnect = async () => {
+});
+const run = async () => {
     try {
-        //  client.connect();
-        const indexKeys ={title:1, category:1};
-const indexOptions = {name:"titleCategory"}
-const result =  toyCollection.createIndex(indexKeys,indexOptions)
+        const toyCollection = client.db('toysDB').collection('toys');
+
+        app.post('/addtoy', async (req, res) => {
+            const toy = req.body;
+            console.log(toy)
+            const result = await toyCollection.insertOne(toy)
+            res.send(result)
+        })
+        
+        
+        app.get('/toysearch/:text', async (req, res) => {
+            const searchText = req.params.text
+            const result = await toyCollection.find({
+                $or: [
+                    { title: { $regex: searchText, Options: 'i' } },
+                    { category: { $regex: searchText, Options: 'i' } },
+                ],
+            }).toArray();
+            res.send(result)
+        })
+        
+        
+        app.get('/alltoy', async (req, res) => {
+            const result = await toyCollection.find().toArray();
+            res.send(result);
+        })
+        app.get('/alltoy/:id', async (req, res) => {
+            const result = await toyCollection.find().toArray();
+            res.send(result);
+        })
+        
+        app.delete('/alltoy/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query);
+            res.send(result)
+        })
+        
+        app.put('/addtoy/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateToys = req.body;
+            const updateDoc = {
+                $set: {
+                    price: body.price,
+                    quantity: body.quantity,
+                    details: body.details,
+                },
+            };
+            const result = await toyCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+        
         console.log("Database Connected successfully ✅");
-    } catch (error) {
-        console.log(error.name, error.message);
+    } finally {
+
     }
 }
-dbConnect();
-const toyCollection = client.db('toysDB').collection('toys');
+run().catch(console.error);
 
-
-
-
-
-app.post('/addtoy', async (req,res) =>{
-    const toy = req.body;
-    console.log(toy)
-    const result = await toyCollection.insertOne(toy)
-    res.send(result)
-})
-
-
-app.get('/toysearch/:text', async(req,res) => {
-    const searchText = req.params.text
-    const result = await toyCollection.find({
-        $or:[
-            {title: {$regex : searchText, Options:'i'}},
-            {category: {$regex : searchText, Options:'i'}},
-        ],
-    }).toArray();
-    res.send(result)
-})
-
-      
-
-
-
-app.get('/addtoy',async (req,res) =>{
-    const result = await toyCollection.find().toArray();
-    res.send(result);
-})
-
-
-
-app.get('/',async (req,res) =>{
-    const result = await toyCollection.find().toArray();
-    res.send(result);
-})
-app.get('/alltoy',async (req,res) =>{
-    const result = await toyCollection.find().toArray();
-    res.send(result);
-})
-app.get('/alltoy/:id',async (req,res) =>{
-    const result = await toyCollection.find().toArray();
-    res.send(result);
-})
-
-app.delete('/addtoy/:id', async(req,res) =>{
-    const id = req.params.id;
-    console.log(id)
-    const query = { _id: new ObjectId(id) }
-    const result = await toyCollection.deleteOne(query);
-    res.send(result)
-})
-
-app.put('/addtoy/:id', async (req,res) => {
-    const id = req.params.id;
-    const filter = { _id: new ObjectId(id) }
-    const updateToys = req.body;
-    const updateDoc ={
-        $set:{
-            price: body.price,
-            quantity: body.quantity,
-            details: body.details,
-        },
-    };
-    const result = await toyCollection.updateOne(filter,updateDoc);
-    res.send(result)
-})
-
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Baby Mart is Running')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Baby Mart Is Running in Port ${port}`)
 })
